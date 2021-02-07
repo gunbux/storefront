76c90ae97e3aaa975c0b5a7cda9a5e2873095bd8
chun <lamchunyu00@gmail.com>
2021-02-07 10:33:23 +0800

    Added react-app

---
+++ store/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="search">
+        <h1>Filters</h1>
+      </div>
+      <div className="listing">
+        <h1>Listings</h1>
+      </div>
+      <div className="desc">
+        <h1>Store description</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
